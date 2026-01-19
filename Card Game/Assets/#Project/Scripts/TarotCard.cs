c8663e29ae8a47ae7066c7fce80f532067c5da94
FanatuXx <antoine.duff@hotmail.com>
2026-01-19 19:39:51 +0100

    Working on Tarot cards

---
+++ Card Game/Assets/#Project/Scripts/TarotCard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+namespace TarotProject
+{
+    [CreateAssetMenu(fileName = "New Tarot Card", menuName = "Tarot Card")]
+    public class TarotCard : ScriptableObject
+    {
+        public string cardName;
+        public Sprite cardSprite;
+        public Sprite cardBack;
+        public string effectDescription;
+    }
+}
